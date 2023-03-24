47cb54b3aa7015a6a4f1dfbff0b0c0f364c61442
Tony Abou Zeidan <tony.azp25@gmail.com>
2023-03-24 12:28:00 -0400

    Message for anthony.

--- tests/test_analysisperformer.py
+++ tests/test_analysisperformer.py
@@ -5,6 +5,8 @@ import evase.structures.analysisperformer as ap
 
 
 if __name__ == '__main__':
+    # TODO: anthony, run this code and figure out why the error is happening
+
     res_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
     test1 = os.path.join(res_path, 'demo')
     test2 = os.path.join(res_path, 'flask_webgoat')
