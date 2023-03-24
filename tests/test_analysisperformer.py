35615a5896e5f527e5d62364bc193816d42c93d1
Tony Abou Zeidan <tony.azp25@gmail.com>
2023-03-24 12:26:43 -0400

    Test on webgoat.

--- tests/test_analysisperformer.py
+++ tests/test_analysisperformer.py
@@ -4,7 +4,10 @@ import pathlib
 import evase.structures.analysisperformer as ap
 
 
-def do_test():
-    apr = ap.AnalysisPerformer("demo", r"U:\courses\SYSC_4907\EvaseAnalysis\tests\resources\demo\backend")
+if __name__ == '__main__':
+    res_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
+    test1 = os.path.join(res_path, 'demo')
+    test2 = os.path.join(res_path, 'flask_webgoat')
+    apr = ap.AnalysisPerformer("demo", test2)
     apr.perform_analysis()
 
