eef94f57b0741e748f7b04f49fb0a1aa3481006e
Tony Abou-Zeidan <tony.azp25@gmail.com>
2023-04-04 15:34:30 -0400

    Filepath testing fix?

--- tests/testutil.py
+++ tests/testutil.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 from evase.structures.projectstructure import ProjectAnalysisStruct
 
-prjroot1_filename = Path('resources/prjstructtest')
-prjroot2_filename = Path('resources/demo')
-prjroot3_filename = Path('resources/webgoat')
-scres1_filename = Path('resources/screstest.py')
-safe1_filename = Path('resources/sql_injection_safe1.py')
-safe2_filename = Path('resources/sql_injection_safe2.py')
-vul1_filename = Path('resources/sql_injection_vul1.py')
-vul2_filename = Path('resources/sql_injection_vul2.py')
-vul3_filename = Path('resources/sql_injection_vul3.py')
-vul4_filename = Path('resources/sql_injection_vul4.py')
-vul5_filename = Path('resources/sql_injection_vul5.py')
-vul6_filename = Path('resources/sql_injection_vul6.py')
+prjroot1_filename = Path(Path(__file__).parent, 'resources', 'prjstructtest')
+prjroot2_filename = Path(Path(__file__).parent, 'resources', 'demo')
+prjroot3_filename = Path(Path(__file__).parent, 'resources', 'webgoat')
+scres1_filename = Path(Path(__file__).parent, 'resources', 'screstest.py')
+safe1_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_safe1.py')
+safe2_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_safe2.py')
+vul1_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul1.py')
+vul2_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul2.py')
+vul3_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul3.py')
+vul4_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul4.py')
+vul5_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul5.py')
+vul6_filename = Path(Path(__file__).parent, 'resources', 'sql_injection_vul6.py')
 
 
 def get_ast_from_filename(filename: str) -> ast.AST:
